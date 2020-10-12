ad63527f7b07ccc97f019f87a26ea5a81a07e084
Paul Razvan Berg <hello@paulrberg.com>
2020-10-13 00:33:35 +0300

    feat: add Exponential.sol

---
+++ contracts/math/ExponentialStorage.sol
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: LPGL-3.0-or-later */
+pragma solidity ^0.7.1;
+
+/**
+ * @title ExponentialStorage
+ * @author Mainframe
+ * @notice The storage interface ancillary to an Exponential contract.
+ */
+abstract contract ExponentialStorage {
+    struct Exp {
+        uint256 mantissa;
+    }
+
+    /**
+     * @notice In Exponential denomination, 1e18 is 1.
+     */
+    uint256 public constant expScale = 1e18;
+    uint256 public constant halfExpScale = expScale / 2;
+    uint256 public constant mantissaOne = expScale;
+}
