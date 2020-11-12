d73aae37099f0fdc2ceb9e0dd79bd39facb9e208
Paul Razvan Berg <hello@paulrberg.com>
2020-11-12 21:10:42 +0200

    refactor: rename 'functionCallWithValue' to 'functionCall'

--- contracts/token/erc20/SafeErc20.sol
+++ contracts/token/erc20/SafeErc20.sol
@@ -55,14 +55,14 @@ library SafeErc20 {
         // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
         // we're implementing it ourselves. We use {Address.functionCall} to perform this call, which verifies that
         // the target address contains contract code and also asserts for success in the low-level call.
-        bytes memory returndata = functionCallWithValue(address(token), data, "ERR_SAFE_ERC20_LOW_LEVEL_CALL");
+        bytes memory returndata = functionCall(address(token), data, "ERR_SAFE_ERC20_LOW_LEVEL_CALL");
         if (returndata.length > 0) {
             /* Return data is optional. */
             require(abi.decode(returndata, (bool)), "ERR_SAFE_ERC20_ERC20_OPERATION");
         }
     }
 
-    function functionCallWithValue(
+    function functionCall(
         address target,
         bytes memory data,
         string memory errorMessage
