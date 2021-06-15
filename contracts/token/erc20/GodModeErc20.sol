98b11f876f17bcb6c3408cbbf53696fbb54857df
Paul Razvan Berg <hello@paulrberg.com>
2021-06-15 13:36:24 +0300

    docs: fix typos in NatSpec in GodModeErc20.sol
    feat: roll v3.2.1

--- contracts/token/erc20/GodModeErc20.sol
+++ contracts/token/erc20/GodModeErc20.sol
@@ -5,7 +5,7 @@ import "./Erc20.sol";
 
 /// @title GodModeErc20
 /// @author Paul Razvan Berg
-/// @notice Implementation that allows anyone to mint or burn tokens belonging to any address.
+/// @notice Allows anyone to mint or burn any amount of tokens to any account.
 /// @dev Strictly for test purposes.
 contract GodModeErc20 is Erc20 {
     /// EVENTS ///
@@ -13,6 +13,8 @@ contract GodModeErc20 is Erc20 {
     event Burn(address indexed holder, uint256 burnAmount);
 
     event Mint(address indexed beneficiary, uint256 mintAmount);
+
+    /// CONSTRUCTOR ///
 
     constructor(
         string memory name_,
@@ -26,7 +28,7 @@ contract GodModeErc20 is Erc20 {
 
     /// @notice Destroys `burnAmount` tokens from `holder`, reducing the token supply.
     /// @param holder The account whose tokens to burn.
-    /// @param burnAmount The amount of fyTokens to destroy.
+    /// @param burnAmount The amount of tokens to destroy.
     function burn(address holder, uint256 burnAmount) external {
         burnInternal(holder, burnAmount);
     }
@@ -34,7 +36,7 @@ contract GodModeErc20 is Erc20 {
     /// @notice Prints new tokens into existence and assigns them to `beneficiary`, increasing the
     /// total supply.
     /// @param beneficiary The account for which to mint the tokens.
-    /// @param mintAmount The amount of fyTokens to print into existence.
+    /// @param mintAmount The amount of tokens to print into existence.
     function mint(address beneficiary, uint256 mintAmount) external {
         mintInternal(beneficiary, mintAmount);
     }
