ee1b3d83deeedfb9da0b8209afbd6d9b76a1a232
Paul Razvan Berg <hello@paulrberg.com>
2021-09-02 15:03:22 +0300

    build: upgrade solidity compiler to v0.8.7
    build: upgrade dev deps
    build: upgrade yarn to v3.0.1
    chore: delete stale yarn resolutions
    chore: git ignore yarn files properly

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -68,7 +68,7 @@ const config: HardhatUserConfig = {
     tests: "./test",
   },
   solidity: {
-    version: "0.8.6",
+    version: "0.8.7",
     settings: {
       metadata: {
         bytecodeHash: "none",
